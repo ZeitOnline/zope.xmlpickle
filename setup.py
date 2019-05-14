d544e6bf816470c675e2f563b5265bab02e55eb6
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2019-05-14 10:45:44 +0200

    Set up tox

--- setup.py
+++ setup.py
@@ -17,36 +17,50 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-setup(name='zope.xmlpickle',
-      version = '3.5.0dev',
+
+TESTS_REQUIRE = [
+    'zope.testrunner',
+]
+
+
+setup(
+      name='zope.xmlpickle',
+      version='3.5.0.dev0',
+      url='https://github.com/zopefoundation/zope.xmlpickle',
+      license='ZPL 2.1',
+      description='XML-based Pickling',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
-      description='XML-based Pickling',
       long_description=(
           read('README.txt')
           + '\n\n' +
           read('CHANGES.txt')
-          ),
-      keywords = "zope3 pickle xml",
-      classifiers = [
+      ),
+      keywords="zope3 pickle xml",
+      classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Zope Public License',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
           'Natural Language :: English',
           'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP'],
-      url='http://cheeseshop.python.org/pypi/zope.xmlpickle',
-      license='ZPL 2.1',
+          'Topic :: Internet :: WWW/HTTP'
+      ],
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['zope'],
       install_requires=[
           'setuptools',
-          ],
-      include_package_data = True,
-      zip_safe = False,
-      )
+      ],
+      extras_require={
+          'test': TESTS_REQUIRE,
+      },
+      include_package_data=True,
+      zip_safe=False,
+ )
